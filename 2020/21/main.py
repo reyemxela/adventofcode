b599f3370a73794620aba1fdfc8926a025123532
Alex Meyer <alexwreyem@gmail.com>
2021-09-04 17:04:13 -0400

    add 2020/21

--- 2020/21/main.py
+++ 2020/21/main.py
@@ -1,20 +1,74 @@
 #!/bin/env python3
 
-with open('input.txt') as f:
-    lines = f.read().splitlines()
+import sys
 
-step1 = 0
-step2 = 0
+def readfile(filename):
+  with open(filename) as f:
+    inp = f.read()
+  return inp
 
-for line in lines:
-    line = line.split(" (")
-    allergens = line[1][:-1].replace(",","").split(" ")[1:]
-    for allergen in allergens:
-        pass
-    print(allergens)
+DEBUG = False
+
+def debug(s=''):
+  if DEBUG:
+    print(s)
+
+def step1(inp):
+  ingredients = []
+  allergens = {}
+
+  for line in inp.splitlines():
+    ings, alls = line.split(' (contains ')
+    ings = ings.split(' ')
+    alls = alls.replace(')', '').split(', ')
+
+    ingredients += ings
+
+    for allergen in alls:
+      allergens.setdefault(allergen, set(ings)).intersection_update(ings)
+
+  allergen_ings = [i for v in allergens.values() for i in v]
+  safe_ings = [i for i in ingredients if i not in allergen_ings]
+  debug(f'{allergens=} {ingredients=} {allergen_ings=} {safe_ings=}')
+  return allergens, len(safe_ings)
+
+def step2(allergens):
+  debug(f'{allergens=}')
+  keys = list(allergens.keys())
+
+  while True:
+    finished = True
+    for i in range(len(keys)):
+      v = allergens[keys[i]]
+      if len(v) != 1:
+        finished = False
+      else:
+        for j in range(len(keys)):
+          if j == i:
+            continue
+          allergens[keys[j]].difference_update(v)
+    if finished:
+      break
+    debug(f'step2: {allergens}')
+
+  return ','.join(i[1].pop() for i in sorted(allergens.items()))
 
 
 
 
-print(F"Step 1: {step1}")
-print(F"Step 2: {step2}")
+def main():
+  filename = "input.txt"
+
+  if len(sys.argv) > 1:
+    filename = sys.argv[1]
+  inp = readfile(filename)
+
+  allergens, step1_out = step1(inp)
+  step2_out = step2(allergens)
+
+  print(f'Step 1: {step1_out}')
+  print(f'Step 2: {step2_out}')
+
+
+if __name__ == '__main__':
+  main()
